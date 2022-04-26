81308df689916cbac729f1d440fbd28d8807ab12
Quốc Thắng <quocthang036@gmail.com>
2022-04-26 11:28:30 +0700

    change UI login - register
    change UI login - register

--- Movie-Application/app/src/main/java/com/android/movie_application/fragment/SearchFragment.java
+++ Movie-Application/app/src/main/java/com/android/movie_application/fragment/SearchFragment.java
@@ -1,4 +1,4 @@
-package com.android.movie_application;
+package com.android.movie_application.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.movie_application.R;
 
 /**
  * A simple {@link Fragment} subclass.
